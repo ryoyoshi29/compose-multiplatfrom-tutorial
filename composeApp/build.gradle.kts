5b7c4a12759d61bf552afa1b392720faa6c70f72
tsuguri <tsuguri4429@gmail.com>
2025-07-05 13:33:54 +0900

    feat: add current time text

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -69,6 +69,10 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
+        }
+        wasmJsMain.dependencies {
+            implementation(npm("@js-joda/timezone", "2.22.0"))
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
--- composeApp/src/commonMain/kotlin/com/example/tutorial/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/tutorial/App.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +13,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -29,6 +37,12 @@ fun App() {
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            Text(
+                text = "Today's date is ${todayDate()}",
+                modifier = Modifier.padding(20.dp),
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center
+            )
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!")
             }
@@ -41,4 +55,12 @@ fun App() {
             }
         }
     }
+}
+
+fun todayDate(): String {
+    fun LocalDateTime.format() = toString().substringBefore('T')
+
+    val now = Clock.System.now()
+    val zone = TimeZone.currentSystemDefault()
+    return now.toLocalDateTime(zone).format()
 }
