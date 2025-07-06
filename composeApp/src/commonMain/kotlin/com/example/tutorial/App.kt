14e2efd6f583e6a3cff1f2ce1db5e32bec438a55
tsuguri <tsuguri4429@gmail.com>
2025-07-06 20:34:28 +0900

    feat: display selected country that current time

--- composeApp/src/commonMain/kotlin/com/example/tutorial/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/tutorial/App.kt
@@ -1,66 +1,92 @@
 package com.example.tutorial
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.datetime.Clock
-import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import tutorial.composeapp.generated.resources.Res
-import tutorial.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+        var showCountries by remember { mutableStateOf(false) }
+        var timeAtLocation by remember { mutableStateOf("No location selected") }
+
         Column(
             modifier = Modifier
+                .padding(20.dp)
                 .safeContentPadding()
                 .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Text(
-                text = "Today's date is ${todayDate()}",
-                modifier = Modifier.padding(20.dp),
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center
+                timeAtLocation,
+                style = TextStyle(fontSize = 20.sp),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)
             )
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+            Row(modifier = Modifier.padding(start = 20.dp, top = 10.dp)) {
+                DropdownMenu(
+                    expanded = showCountries,
+                    onDismissRequest = { showCountries = false }
+                ) {
+                    countries().forEach { (name, zone) ->
+                        DropdownMenuItem(
+                            text = { Text(name) },
+                            onClick = {
+                                timeAtLocation = currentTimeAt(name, zone) ?: ""
+                                showCountries = false
+                            }
+                        )
+                    }
                 }
+            }
+            Button(
+                onClick = { showCountries = !showCountries },
+                modifier = Modifier.padding(start = 20.dp, top = 10.dp)
+            ) {
+                Text("Select Location")
             }
         }
     }
 }
 
-fun todayDate(): String {
-    fun LocalDateTime.format() = toString().substringBefore('T')
+fun currentTimeAt(location: String, zone: TimeZone): String {
+    fun LocalTime.formatted() = "$hour:$minute:$second"
 
-    val now = Clock.System.now()
-    val zone = TimeZone.currentSystemDefault()
-    return now.toLocalDateTime(zone).format()
+    val time = Clock.System.now()
+    val localTime = time.toLocalDateTime(zone).time
+    return "The time in $location is ${localTime.formatted()}"
 }
+
+data class Country(val name: String, val zone: TimeZone)
+
+fun countries() = listOf(
+    Country("Japan", TimeZone.of("Asia/Tokyo")),
+    Country("France", TimeZone.of("Europe/Paris")),
+    Country("Mexico", TimeZone.of("America/Mexico_City")),
+    Country("Indonesia", TimeZone.of("Asia/Jakarta")),
+    Country("Egypt", TimeZone.of("Africa/Cairo")),
+)
